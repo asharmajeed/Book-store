a7d8c83c8fa12b9e59e480566657bb866929f272
asharmajeed <asharmajeed40@gmail.com>
2024-10-18 18:43:41 +0500

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://nodeninja:test1234@nodejs.t4gb2xw.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Nodejs';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
